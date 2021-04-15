a41632a38e3b4d7f3c2fbc32baa255c35a2c6a70
Wasim Algamal <walgamal@umich.edu>
2021-04-15 16:06:13 -0400

    added test to check for unit test for testPositive

--- testSelectSort.java
+++ testSelectSort.java
@@ -10,7 +10,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class testSelectSort {
-	public SelectionSort sorted = new SelectionSort(); 
+	public SelectionSort sorted = new SelectionSort();
 	@Test
 	public void test() {
 		//testPositive();
@@ -21,7 +21,7 @@ public class testSelectSort {
 
     public testSelectSort() {
     }
-    
+
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -29,22 +29,22 @@ public class testSelectSort {
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
+
         /** add tests to check for this unit test **/
-        arr = sorted.basicSelectionSort(arr);
-        
+				arr = sorted.basicSelectionSort(arr);
+
         for(int i = 0; i < arr.length; ++i) {
         	assertTrue("Not a match!", arr[i] == Sortedarr[i]);
         }
     }
-    
+
     public void testNegative(){
     	int[] arr = new int[5];
         arr[0] = -8;
@@ -52,22 +52,22 @@ public class testSelectSort {
         arr[2] = -7;
         arr[3] = -10;
         arr[4] = -2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = -10;
         Sortedarr[1] = -9;
         Sortedarr[2] = -8;
         Sortedarr[3] = -7;
         Sortedarr[4] = -2;
-        
+
         /** add tests to check for this unit test **/
         arr = sorted.basicSelectionSort(arr);
-        
+
         for(int i = 0; i < arr.length; ++i) {
         	assertTrue("Not a match!", arr[i] == Sortedarr[i]);
         }
     }
-    
+
     public void testMixed(){
     	int[] arr = new int[5];
         arr[0] = -8;
@@ -75,22 +75,22 @@ public class testSelectSort {
         arr[2] = -7;
         arr[3] = 10;
         arr[4] = -2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = -8;
         Sortedarr[1] = -7;
         Sortedarr[2] = -2;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
+
         /** add tests to check for this unit test **/
         arr = sorted.basicSelectionSort(arr);
-        
+
         for(int i = 0; i < arr.length; ++i) {
         	assertTrue("Not a match!", arr[i] == Sortedarr[i]);
         }
     }
-    
+
     public void testDuplicates(){
     	int[] arr = new int[5];
         arr[0] = 8;
@@ -98,17 +98,17 @@ public class testSelectSort {
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 8;
         Sortedarr[4] = 10;
-        
+
         /** add tests to check for this unit test **/
         arr = sorted.basicSelectionSort(arr);
-        
+
         for(int i = 0; i < arr.length; ++i) {
         	assertTrue("Not a match!", arr[i] == Sortedarr[i]);
         }
